9e80ccbf86f7d0bcceaf4d09dad41043e4568b20
leonardobianchii <leo-bianchi@hotmail.com>
2025-05-23 00:24:41 -0300

    Adicionando dockerfile

--- api-mottu/src/main/java/br/monitoramento/motu/api_mottu/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.monitoramento.motu.api_mottu.exception;
-
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- api-mottu/src/main/java/br/monitoramento/motu/api_mottu/repository/SensorRFIDRepository.java
+++
@@ -1,9 +0,0 @@
-package br.monitoramento.motu.api_mottu.repository;
-
-import br.monitoramento.motu.api_mottu.model.SensorRFID;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SensorRFIDRepository extends JpaRepository<SensorRFID, Long> {
-}
